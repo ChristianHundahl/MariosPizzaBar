0993bc757c785e669d92ef199008cd1e194af3cf
Daniell <daniellrehder@gmail.com>
2021-04-14 15:14:17 +0200

    2. commit @ Ordreliste

--- Pizzabar/Transactions/Ordreliste.java
+++ Pizzabar/Transactions/Ordreliste.java
@@ -1,6 +1,28 @@
 package Transactions;
+import java.util.ArrayList;
 
 public class Ordreliste {
+    ArrayList<String> orderList = new ArrayList<String>();
+
+    public void showList(String[] listToPrint){
+        for (int i = 0; i < listToPrint.length; i++){
+            System.out.println(listToPrint[i]);
+        }
+    }
+
+    public void addOrder(){
+
+    }
+
+    public void removeOrder(){
+
+    }
+
+
+
+
+
+
     //ArrayList w/ ordrerlist
     //getOrder()
     //removeOrder()
