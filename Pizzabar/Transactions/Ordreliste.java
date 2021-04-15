a877325bea0fdd84353cd2f1972a0420a938743b
Daniell <daniellrehder@gmail.com>
2021-04-15 09:26:17 +0200

    3. Push @ Ordreliste

--- Pizzabar/Transactions/Ordreliste.java
+++ Pizzabar/Transactions/Ordreliste.java
@@ -1,14 +1,31 @@
 package Transactions;
+import ALaCarte.Pizza;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ordreliste {
     ArrayList<Ordre> orderList = new ArrayList<Ordre>();
+    Scanner in = new Scanner(System.in);
 
     public void showList(){
         System.out.println(orderList.toString());
     }
 
     public void addOrder(String kundenavn, String afhentningstidspunkt, String kommentar, double total){
+        System.out.println("Kundenavn: ");
+        String navn = in.nextLine();
+        System.out.println("Afhentningstidspunkt: ");
+        String afhentTid = in.nextLine();
+        System.out.println("Kommentar: ");
+        String kom = in.nextLine();
+        System.out.println("Total: ");
+        double sum = in.nextDouble();
+        in.nextLine();
+
+        System.out.println();
+        System.out.println("Ordren er tilføjet.");
+
         Ordre tempOrdre = new Ordre(kundenavn, afhentningstidspunkt, kommentar, total);
         orderList.add(tempOrdre);
     }
